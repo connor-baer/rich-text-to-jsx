bd4ea91c738ee33aad04ceae23de9139e2dbe229
Connor Bär <connor.baer@sumup.com>
2019-01-20 17:48:29 +0100

    Add fixtures

--- __fixtures__/index.js
+++ __fixtures__/index.js
@@ -1,84 +1,128 @@
-/* eslint-disable import/prefer-default-export, max-len */
-export const richText = {
+/* eslint-disable max-len */
+export function createDocument(content) {
+  return {
+    data: {},
+    nodeType: 'document',
+    content
+  };
+}
+
+export const headingOne = {
   data: {},
   content: [
     {
       data: {},
-      content: [
-        {
-          data: {},
-          marks: [],
-          value: 'Simple rich text supports ',
-          nodeType: 'text'
-        },
-        {
-          data: {},
-          marks: [
-            {
-              type: 'bold'
-            }
-          ],
-          value: 'bold',
-          nodeType: 'text'
-        },
-        {
-          data: {},
-          marks: [],
-          value: ' & ',
-          nodeType: 'text'
-        },
-        {
-          data: {},
-          marks: [
-            {
-              type: 'italic'
-            }
-          ],
-          value: 'italic',
-          nodeType: 'text'
-        },
-        {
-          data: {},
-          marks: [],
-          value: ' text, ',
-          nodeType: 'text'
-        },
-        {
-          data: {
-            uri: 'https://sumup.com'
-          },
-          content: [
-            {
-              data: {},
-              marks: [],
-              value: 'links',
-              nodeType: 'text'
-            }
-          ],
-          nodeType: 'hyperlink'
-        },
-        {
-          data: {},
-          marks: [],
-          value: ', and line breaks.',
-          nodeType: 'text'
-        }
-      ],
-      nodeType: 'paragraph'
-    },
-    {
-      data: {},
-      content: [
-        {
-          data: {},
-          marks: [],
-          value:
-            'It does not support headers, images, tables, and other block-level elements.',
-          nodeType: 'text'
-        }
-      ],
-      nodeType: 'paragraph'
+      marks: [],
+      value: 'This is a first-level heading',
+      nodeType: 'text'
     }
   ],
-  nodeType: 'document'
+  nodeType: 'heading-1'
+};
+
+export const headingThree = {
+  data: {},
+  content: [
+    {
+      data: {},
+      marks: [],
+      value: 'This is a third-level heading',
+      nodeType: 'text'
+    }
+  ],
+  nodeType: 'heading-3'
+};
+
+export const text = {
+  data: {},
+  marks: [],
+  value: 'This is normal text.',
+  nodeType: 'text'
+};
+
+export const bold = {
+  data: {},
+  marks: [
+    {
+      type: 'bold'
+    }
+  ],
+  value: 'This is bold text.',
+  nodeType: 'text'
+};
+
+export const italic = {
+  data: {},
+  marks: [
+    {
+      type: 'italic'
+    }
+  ],
+  value: 'This is italic text.',
+  nodeType: 'text'
+};
+
+export const underline = {
+  data: {},
+  marks: [
+    {
+      type: 'underline'
+    }
+  ],
+  value: 'This is underlined text.',
+  nodeType: 'text'
+};
+
+export const paragraph = {
+  data: {},
+  content: [text, bold, italic, underline],
+  nodeType: 'paragraph'
+};
+
+export const blockquote = {
+  data: {},
+  content: [paragraph],
+  nodeType: 'blockquote'
+};
+
+export const listItem = {
+  data: {},
+  content: [paragraph],
+  nodeType: 'list-item'
+};
+
+export const unorderedList = {
+  data: {},
+  content: [listItem, listItem, listItem],
+  nodeType: 'unordered-list'
+};
+
+export const hr = {
+  data: {},
+  content: [],
+  nodeType: 'hr'
+};
+
+export const embeddedAsset = {
+  data: {
+    target: {
+      id: '4fgGUXCJXWOQUAEQqCS8MW',
+      updatedAt: '2019-01-14T04:57:27.049Z',
+      title: 'Random photo from Unsplash.com',
+      file: {
+        url: 'https://source.unsplash.com/random/800x500',
+        details: {
+          size: 240963,
+          image: {
+            width: 800,
+            height: 500
+          }
+        },
+        fileName: 'random-unsplash.jpg',
+        contentType: 'image/jpg'
+      }
+    }
+  },
+  content: [],
+  nodeType: 'embedded-asset-block'
 };
--- src/RichText.spec.js
+++ src/RichText.spec.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { create } from 'react-test-renderer';
 
-import { richText } from '../__fixtures__';
+import {
+  createDocument,
+  headingOne,
+  paragraph,
+  unorderedList,
+  blockquote,
+  hr
+} from '../__fixtures__';
 
 import RichText from './RichText';
 
@@ -9,6 +16,13 @@ describe('RichText', () => {
   // !TODO: write your tests.
   describe('styles', () => {
     it('should render with default styles', () => {
+      const richText = createDocument([
+        headingOne,
+        paragraph,
+        unorderedList,
+        hr,
+        blockquote
+      ]);
       const actual = create(<RichText richText={richText} />);
       expect(actual).toMatchSnapshot();
     });
