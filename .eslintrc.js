7fcb4b76a649c4921f6c838c149a2e94509d7e31
Connor Bär <connor.baer@sumup.com>
2019-09-05 22:17:02 -0300

    chore(deps): Upgrade dev dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,12 @@
-module.exports = require('@sumup/foundry/eslint').react;
+const {
+  react: baseConfig,
+  overwritePresets
+} = require('@sumup/foundry/eslint');
+
+const customConfig = {
+  rules: {
+    'notice/notice': 'off'
+  }
+};
+
+module.exports = overwritePresets(baseConfig, customConfig);
